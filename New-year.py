db5b916fc00524d3fcbb75597ff80b41dc8ef29b
Mafiyahunter <110088114+Mafiyahunter@users.noreply.github.com>
2022-12-31 20:24:23 +0530

    Update New-year.py

--- New-year.py
+++ New-year.py
@@ -1,21 +1,26 @@
-#HAPPY NEW YEAR 2023
+import time 
+from random import randint
 
-import time
-import random
-for i in range (1,45):
-	print (" ")
-s=" " 
-for i in range(1,10000):
-	count=random.randint(1,100)
-	while count>0:
-		s+="  "
-		count-=1 
-		
-	if 1%10==0:
-		print( f"{s} HAPPY NEW YEAR 2023")
-	else:
-		print (s+"*")
-	s=" "
-	time.sleep(0.3)
+for i in range(1,45):
+      print(" ")
+      
+      s= " "
+      
+      
+for i in range(1,10000)
+      count= range(1,100)
+      while (count >0):
+      	s += " " 
+      count- = 1
+      
+      
+      if(1%10==0):
+      	print(s+ "happy new year")
+      else:
+      	print(s+"*")
+      
+      s= " "
+      time.sleep(0.3)
+
 	
 	
